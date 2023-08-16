131c1930c6c2fed8fa92b9fe396986432316cd18
Merlin <merlin.cosentino@outlook.es>
2023-08-16 18:36:09 -0300

    Primer commit de HeroesApp

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,21 @@
+import { HeroCard } from './';
+import { getHeroesByPublisher } from '../helpers';
+import { useMemo } from 'react';
+
+export const HeroList = ({publisher}) => {
+
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-3">
+        {
+            heroes.map( heroe => (
+                <HeroCard
+                    key={heroe.id}
+                    {...heroe}
+                />
+            ))
+        }
+    </div>
+  )
+}
