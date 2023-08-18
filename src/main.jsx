080e4ef8d050d6a8643948ded16b6a149da8220d
Merlin <merlin.cosentino@outlook.es>
2023-08-18 16:21:48 -0300

    basename actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/heroes-app'>
+    <BrowserRouter basename='https://merlincosentino.github.io/heroes-app'>
       <HeroesApp />
     </BrowserRouter>
   </React.StrictMode>
