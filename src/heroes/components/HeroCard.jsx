767bafac91f02d02b2bb2127b77aec5df4553c1d
Merlin <merlin.cosentino@outlook.es>
2023-08-18 15:56:38 -0300

    Imágenes y reload arreglado 5

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -47,7 +47,7 @@ export const HeroCard = ({
                             <small className="text-muted">{first_appearance}</small>
                         </p>
 
-                        <Link to={`hero/${id}`}>
+                        <Link to={`/hero/${id}`}>
                             Mas...
                         </Link>
 
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -21,7 +21,7 @@ export const HeroPage = () => {
     <div className="row mt-5 animate__animated animate__fadeInLeft">
       <div className="col-4">
         <img
-          src={`heroes/${id}.jpg`}
+          src={`/heroes/${id}.jpg`}
           alt={hero.superhero}
           className="img-thumbnail"
         />
